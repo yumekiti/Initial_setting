60751ba82e75f7619e2bd40d2dd695cce7f2e46c
yumekiti <yumekiti120440@gmail.com>
2021-04-25 18:32:36 +0900

    init

--- initial_setting.sh
+++ initial_setting.sh
@@ -2,50 +2,50 @@ sudo apt update && sudo apt -y upgrade ;
 # パッケージ一覧アップデート & パッケージアップデート
 
 if !(type vim > /dev/null 2>&1) ; then
-sudo apt -y install vim
+sudo apt -y install vim ;
 fi
 # テキストエディタ
 
 if !(type curl > /dev/null 2>&1) ; then
-sudo apt -y install  curl 
+sudo apt -y install  curl ;
 fi
 # httpリクエスト
 
 if !(type screen > /dev/null 2>&1) ; then
-sudo apt -y install screen 
+sudo apt -y install screen ;
 fi
 # 画面分割
 
 if !(type dconf-editor > /dev/null 2>&1) ; then
-sudo apt -y install dconf-editor 
+sudo apt -y install dconf-editor ;
 fi
 # dock透明度やら
 
 if !(type gnome-tweaks > /dev/null 2>&1) ; then
-sudo apt -y install gnome-tweaks
+sudo apt -y install gnome-tweaks ;
 fi
 # デスクトップカスタマイズ
 
 if !(type chrome-gnome-shell > /dev/null 2>&1) ; then
-sudo apt -y install chrome-gnome-shell
+sudo apt -y install chrome-gnome-shell ;
 fi
 # デスクトップカスタマイズ
 
 if !(type java > /dev/null 2>&1) ; then
-sudo apt -y install openjdk-14-jdk
+sudo apt -y install openjdk-14-jdk ;
 fi 
 # java
 
 # sudo timedatectl set-local-rtc true ;
 # win10 9時間ずらす
 
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true ;
 # Dock にゴミ箱を表示する
 
-gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false ;
 # desktop にゴミ箱を非表示する
 
-gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false ;
 # desktop にhomeを非表示する
 
 dconf write /org/gnome/shell/extensions/panel-date-format/format "'%Y年%m月%d日\(%a\) %H時%M分%S秒'" ;
@@ -73,11 +73,15 @@ curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb ;
 sudo apt install ./vscode.deb ;
 # vscode install
 
-wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > packages.google.gpg
-sudo install -o root -g root -m 644 packages.google.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.google.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+curl -L https://discord.com/api/download?platform=linux&format=deb -o discode.deb ;
+sudo apt install ./discode.deb ;
+# discode
 
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > packages.google.gpg ;
+sudo install -o root -g root -m 644 packages.google.gpg /etc/apt/trusted.gpg.d/ ;
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.google.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' ;
+
+sudo apt-get install apt-transport-https ;
+sudo apt-get update ;
+sudo apt-get install -y google-chrome-stable ;
 # chrome
